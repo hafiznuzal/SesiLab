1923a0c6a99664500365b518378099733adfd100
hafiznuzal <hafiz_nuzal@yahoo.co.id>
2015-08-21 10:54:27 +0700

    update controller and view

--- application/controllers/HalUtama.php
+++ application/controllers/HalUtama.php
@@ -13,45 +13,30 @@ class HalUtama extends CI_Controller {
 		$this->load->view('index');
 	}
 
-	function add()
+	function absen()
 	{
-			
-		$data['NO_PEMILIH']=$this->input->POST('NO_PEMILIH');
-		$data['NO_KTP']=$this->input->POST('NO_KTP');
-		$data['STATUS']=$this->input->POST('0');
-		#$status = '3';
-		$this->load->model('model_addpemilih');
-		$status=$this->model_addpemilih->insert_entry($data);
-		if ($status == '0')
-		{
-			$parsing_data =array('stat'=> "gagal");
-			$this->load->view('form_addpemilih',$parsing_data);
-		}
-		else if ($status == '1')
-		{
-			$parsing_data =array('stat'=> "ok");
-			$this->load->view('form_addpemilih',$parsing_data);
-		}
+		$this->load->view('navigate');
+		$this->load->view('index');	
+		
 	}
-	function delete($NO_PEMILIH)
+	function upload($NO_PEMILIH)
 	{
 		
-		$this->load->model('model_addpemilih');
-		$delete=$this->model_addwni->delete($NO_KTP);
-		if($delete)
-		{
-			echo "data berhasil dihapus <br>";
+		$this->load->view('navigate');
+		$this->load->view('upload');
+		
+	}
+	function download($no_ktp)
+	{
+		$this->load->view('navigate');
+		$this->load->view('download');	
+	}
 
-			echo "<a href=".site_url('addwni/show/').">kembali</a>";
-		}
-		
-	}
-	function show_specific($no_ktp)
+	function manajemen($no_ktp)
 	{
-		$this->load->model('model_addpemilih');
-		$data=$this->model_addpemilih->show_specific($NO_PEMILIH);
-		$parsing_data = array('data'=> $data);
-		$this->load->view('show_wni',$parsing_data);	
-	}
+		$this->load->view('navigate');
+		$this->load->view('manajemen');	
+	}	
+
 }
 ?>
